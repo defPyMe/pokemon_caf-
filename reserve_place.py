f448943585e9fec71f34f6b75a29ee7e3cf79658
Luca Cavazzini <91831512+defPyMe@users.noreply.github.com>
2024-03-12 15:32:56 +0100

    adding the comments

--- reserve_place.py
+++ reserve_place.py
@@ -4,9 +4,14 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 import sys, os
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+time_start = time.time()
 
-
+def scroll(driver):
+    driver.execute_script("window.scrollTo(100,3200);")
 
 
 
@@ -20,27 +25,46 @@ def create_booking(day_of_month, num_of_guests):
     website = "https://reserve.pokemon-cafe.jp/"
     chrome_options = webdriver.EdgeOptions()
     chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+    chrome_options.add_argument('--disk-cache-dir=/path/to/cache')
+    chrome_options.page_load_strategy = 'eager'
     chromedriver = "edgedriver"
     driver = webdriver.Edge( options=chrome_options)
     driver.get(website)
 
     try:
         # 席の予約 HTML 1 - Make a reservation
-        driver.execute_script("window.scrollTo(100,3200);")
+        scroll(driver)
    
 
         # 席の予約 HTML 2 - Agree T&C
         b = driver.find_element(By.XPATH, "//*[@class='agreeChecked']").click()
         c = driver.find_element(By.XPATH, "//*[@class='button']").click()
 
-        # 席の予約 HTML 3 - Select number of guest
-        d = select = Select(driver.find_element(By.NAME, 'guest'))
-        e = select.select_by_index(num_of_guests)
+        #accessing teh button class arrow down "button arrow-down"
+        time.sleep(2)
+        scroll(driver)
+        driver.find_element(By.XPATH, "//*[@class='button arrow-down']").click()
+        #number of guests
 
-        # 席の予約 HTML 4 - Select from calendar
-        f = driver.find_element(By.XPATH, "//*[contains(text(), '次の月を見る')]").click()
-        g = driver.find_element(By.XPATH, "//*[contains(text(), " + str(day_of_month) + ")]").click()
-        h = driver.find_element(By.XPATH, "//*[@class='button']").click()
+        #old way to do it 
+
+
+
+        select = Select(driver.find_element(By.NAME, 'guest'))
+        select.select_by_index(num_of_guests)
+        
+        
+        
+        #select.select_by_index(num_of_guests)
+        driver.find_element(By.XPATH, "//*[contains(text(), '次の月を見る')]").click()
+        driver.find_element(By.XPATH, "//*[contains(text(), " + str(day_of_month) + ")]").click()
+        driver.find_element(By.XPATH, "//*[@class='button']").click()
+
+        #clicking on hours
+        driver.find_element(By.XPATH, "//*[contains(text(), '20:05')]").click()
+
+
         
     except Exception as e :
         print(e)
@@ -55,3 +79,12 @@ day_of_month='11'
 num_of_guests=2
 
 [create_booking(day_of_month, num_of_guests) for x in range(num_iterations)]
+
+time_end = time.time()
+print(f"elapsed time is {time_end - time_start}")
+
+
+
+#get_url = driver.current_url 
+#print("The current url is:"+str(get_url))
+#
